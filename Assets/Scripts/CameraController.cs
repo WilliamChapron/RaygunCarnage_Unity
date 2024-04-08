f8a11d3029f680c27164c84609f2579f87a46f55
William's Student <102622446+WilliamChapron@users.noreply.github.com>
2024-04-08 11:26:08 +0200

    Camera First Stable Version
    Camera First Stable Version

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -74,119 +74,150 @@ public class CameraController : MonoBehaviour
         farthestPlayerPositions.Add(new Vector3(0.0f, 0.0f, 0.0f));
 
 
-        // sa prend 7 
-        int i = 0;
-        foreach (Vector3 pos in secondPositionsList)
-        {
-            Debug.Log("ITERATION : " + i);
+        
+        //int i = 0;
+        //foreach (Vector3 pos in secondPositionsList)
+        //{
+        //    Debug.Log("ITERATION : " + i);
 
-            Vector3 position = farthestPlayerPositions[0];
-            if (Mathf.Abs(pos.x) >= Mathf.Abs(farthestPlayerPositions[0].x))
-            {
-                Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.x) + " > " + Mathf.Abs(farthestPlayerPositions[0].x));
-                position.x = pos.x;
-            }
-            else
-            {
-                Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.x) + " < " + Mathf.Abs(farthestPlayerPositions[0].x));
-            }
+        //    Vector3 position = farthestPlayerPositions[0];
+        //    if (Mathf.Abs(pos.x) >= Mathf.Abs(farthestPlayerPositions[0].x))
+        //    {
+        //        Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.x) + " > " + Mathf.Abs(farthestPlayerPositions[0].x));
+        //        position.x = pos.x;
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.x) + " < " + Mathf.Abs(farthestPlayerPositions[0].x));
+        //    }
 
-            if (Mathf.Abs(pos.y) >= Mathf.Abs(farthestPlayerPositions[0].y))
-            {
-                Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.y) + " > " + Mathf.Abs(farthestPlayerPositions[0].y));
-                position.y = pos.y;
-            }
-            else
-            {
-                Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.y) + " < " + Mathf.Abs(farthestPlayerPositions[0].y));
-            }
+        //    if (Mathf.Abs(pos.y) >= Mathf.Abs(farthestPlayerPositions[0].y))
+        //    {
+        //        Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.y) + " > " + Mathf.Abs(farthestPlayerPositions[0].y));
+        //        position.y = pos.y;
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.y) + " < " + Mathf.Abs(farthestPlayerPositions[0].y));
+        //    }
 
-            if (Mathf.Abs(pos.z) >= Mathf.Abs(farthestPlayerPositions[0].z))
-            {
-                Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.z) + " > " + Mathf.Abs(farthestPlayerPositions[0].z));
+        //    if (Mathf.Abs(pos.z) >= Mathf.Abs(farthestPlayerPositions[0].z))
+        //    {
+        //        Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.z) + " > " + Mathf.Abs(farthestPlayerPositions[0].z));
 
-                position.z = pos.z;
-            }
-            else
-            {
-                Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.z) + " < " + Mathf.Abs(farthestPlayerPositions[0].z));
-            }
+        //        position.z = pos.z;
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.z) + " < " + Mathf.Abs(farthestPlayerPositions[0].z));
+        //    }
 
-            farthestPlayerPositions[0] = position;
+        //    farthestPlayerPositions[0] = position;
 
-            Debug.Log("farthestPlayerPositions après mise à jour : " + farthestPlayerPositions[0]);
-            i++;
-        }
+        //    Debug.Log("farthestPlayerPositions après mise à jour : " + farthestPlayerPositions[0]);
+        //    i++;
+        //}
 
-        foreach (Vector3 pos in secondPositionsList)
-        {
-            Debug.Log("ITERATION : " + i);
+        //foreach (Vector3 pos in secondPositionsList)
+        //{
+        //    Debug.Log("ITERATION : " + i);
 
-            Vector3 position = farthestPlayerPositions[1];
-            if (Mathf.Abs(pos.x) >= Mathf.Abs(farthestPlayerPositions[1].x))
-            {
-                Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.x) + " > " + Mathf.Abs(farthestPlayerPositions[1].x));
-                if (pos.x != farthestPlayerPositions[0].x)
-                {
-                    position.x = pos.x;
-                }   
-            }
-            else
-            {
-                Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.x) + " < " + Mathf.Abs(farthestPlayerPositions[1].x));
-            }
+        //    Vector3 position = farthestPlayerPositions[1];
+        //    if (Mathf.Abs(pos.x) >= Mathf.Abs(farthestPlayerPositions[1].x))
+        //    {
+        //        Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.x) + " > " + Mathf.Abs(farthestPlayerPositions[1].x));
+        //        if (pos.x != farthestPlayerPositions[0].x)
+        //        {
+        //            position.x = pos.x;
+        //        }   
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.x) + " < " + Mathf.Abs(farthestPlayerPositions[1].x));
+        //    }
 
-            if (Mathf.Abs(pos.y) >= Mathf.Abs(farthestPlayerPositions[1].y))
-            {
-                Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.y) + " > " + Mathf.Abs(farthestPlayerPositions[1].y));
-                if (pos.y != farthestPlayerPositions[0].y)
-                {
-                    position.y = pos.y;
-                }
-            }
-            else
-            {
-                Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.y) + " < " + Mathf.Abs(farthestPlayerPositions[1].y));
-            }
+        //    if (Mathf.Abs(pos.y) >= Mathf.Abs(farthestPlayerPositions[1].y))
+        //    {
+        //        Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.y) + " > " + Mathf.Abs(farthestPlayerPositions[1].y));
+        //        if (pos.y != farthestPlayerPositions[0].y)
+        //        {
+        //            position.y = pos.y;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.y) + " < " + Mathf.Abs(farthestPlayerPositions[1].y));
+        //    }
 
-            if (Mathf.Abs(pos.z) >= Mathf.Abs(farthestPlayerPositions[1].z))
-            {
-                Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.z) + " > " + Mathf.Abs(farthestPlayerPositions[1].z));
-                if (pos.z != farthestPlayerPositions[0].z)
-                {
-                    position.z = pos.z;
-                }
+        //    if (Mathf.Abs(pos.z) >= Mathf.Abs(farthestPlayerPositions[1].z))
+        //    {
+        //        Debug.Log("Pos du player supérieur a pos du tableau, regarde ->  " + Mathf.Abs(pos.z) + " > " + Mathf.Abs(farthestPlayerPositions[1].z));
+        //        if (pos.z != farthestPlayerPositions[0].z)
+        //        {
+        //            position.z = pos.z;
+        //        }
                 
-            }
-            else
-            {
-                Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.z) + " < " + Mathf.Abs(farthestPlayerPositions[1].z));
-            }
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("Pos du player inferieur a celle du tableau, regarde ->  " + Mathf.Abs(pos.z) + " < " + Mathf.Abs(farthestPlayerPositions[1].z));
+        //    }
 
-            farthestPlayerPositions[1] = position;
+        //    farthestPlayerPositions[1] = position;
 
-            //Debug.Log("farthestPlayerPositions après mise à jour : " + farthestPlayerPositions[1]);
-            i++;
+        //    //Debug.Log("farthestPlayerPositions après mise à jour : " + farthestPlayerPositions[1]);
+        //    i++;
+        //}
+
+
+
+
+
+
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+
+        //    GameObject lightObject = new GameObject("BrightLight");
+        //    Light lightComponent = lightObject.AddComponent<Light>();
+        //    lightComponent.color = Color.red;
+        //    lightComponent.intensity = 50f;
+
+        //    // Définir la position de la lumière
+        //    lightObject.transform.position = new Vector3(farthestPlayerPositions[0].x, farthestPlayerPositions[0].y, farthestPlayerPositions[0].z);
+
+        //    GameObject lightObject2 = new GameObject("BrightLight2");
+        //    Light lightComponent2 = lightObject2.AddComponent<Light>();
+        //    lightComponent2.color = Color.blue;
+        //    lightComponent2.intensity = 50f;
+
+        //    // Définir la position de la lumière
+        //    lightObject.transform.position = new Vector3(farthestPlayerPositions[1].x, farthestPlayerPositions[1].y, farthestPlayerPositions[1].z);
+        //}
+
+
+        float distance = Vector3.Distance(playerObjects[0].transform.position, playerObjects[1].transform.position);
+        float newFOV = distance * 0.8f; // Inverse de la distance
+        //if (newFOV > 50.0f && newFOV < 140.0f)
+        //{
+        //    Debug.Log(" Fov " + newFOV);
+        //    GetComponent<Camera>().fieldOfView = newFOV;
+        //    Debug.Log("ddd");
+        //}
+        Debug.Log(" Fov " + newFOV);
+        foreach (Vector3 pos in positionsList)
+        {
+            averageLookAt += pos;
         }
+        averageLookAt /= positionsList.Count;
 
-
-
-
-
-
-        //Debug.Log("La première valeur la plus grande en x est " + farthestPlayerPositions[0].x);
-        //Debug.Log("La première valeur la plus grande en y est " + farthestPlayerPositions[0].y);
-        //Debug.Log("La première valeur la plus grande en z est " + farthestPlayerPositions[0].z);
-
-        //Debug.Log("La deuxième valeur la plus grande en x est " + farthestPlayerPositions[1].x);
-        //Debug.Log("La deuxième valeur la plus grande en y est " + farthestPlayerPositions[1].y);
-        //Debug.Log("La deuxième valeur la plus grande en z est " + farthestPlayerPositions[1].z);
-
-
-
-
-
-
+        //transform.LookAt(averageLookAt, Vector3.up);
+        float newYPosition;
+        if (newFOV > 25.0f && newFOV < 140.0f)
+        {
+            newYPosition = newFOV;
+            Vector3 cameraPosition = new Vector3(averageLookAt.x - 20.0f, newYPosition, averageLookAt.z); ;
+            transform.position = cameraPosition;
+        }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -205,34 +236,11 @@ public class CameraController : MonoBehaviour
             lightComponent2.intensity = 50f;
 
             // Définir la position de la lumière
-            lightObject.transform.position = new Vector3(farthestPlayerPositions[1].x, farthestPlayerPositions[1].y, farthestPlayerPositions[1].z);
-        }
-
-
-        float distance = Vector3.Distance(farthestPlayerPositions[0], farthestPlayerPositions[1]);
-        float newFOV = distance * 1f; // Inverse de la distance
-
-        if (newFOV > 8)
-        {
-            Debug.Log(" Fov " + newFOV);
-            GetComponent<Camera>().fieldOfView = newFOV;
-            Debug.Log("ddd");
+            lightObject.transform.position = averageLookAt;
         }
 
 
 
-        foreach (Vector3 pos in positionsList)
-        {
-            averageLookAt += pos;
-        }
-
-        averageLookAt /= positionsList.Count;
-
-        Debug.Log("Position moyenne : " + averageLookAt);
-        Debug.Log("Position PLAYER 1 : " + playerObjects[0].transform.position);
-        Debug.Log("Position PLAYER 2 : " + playerObjects[1].transform.position);
-
-        transform.LookAt(averageLookAt, Vector3.up);
 
         //GetComponent<Camera>().fieldOfView -= _zoomSpeed * Time.deltaTime;
 
@@ -242,6 +250,7 @@ public class CameraController : MonoBehaviour
         //Vector3 movement = new Vector3(0f, 0f, verticalInput) * _moveSpeed * Time.deltaTime;
         //transform.position = average;
     }
+
 
 
 
