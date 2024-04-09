cf405001a1c78a880adc801ffe323a4769e01865
Mattéo <117280997+urrikki@users.noreply.github.com>
2024-04-09 10:51:53 +0200

    Loading Screen

---
+++ Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Slider LoadingBaFill;
+    public GameObject button;
+    public GameObject slider;
+    public void LoadScene(int sceneId)
+    {
+        button.SetActive(false);
+        slider.SetActive(true);
+        StartCoroutine(LoadSceneAsnc(sceneId));
+    }
+
+    IEnumerator LoadSceneAsnc(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone) 
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadingBaFill.value = progressValue;
+        
+            yield return null;        
+        }
+    }
+}
