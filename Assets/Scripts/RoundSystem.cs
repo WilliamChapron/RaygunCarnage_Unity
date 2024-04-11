fee1338fb9451e6411cae3b284dc0a6efb73f61c
KoniKunHugo <117296330+KoniKunHugo@users.noreply.github.com>
2024-04-11 09:59:01 +0200

    Rounds Text branch created
    (Issue Active is bugged, whilst in state False, it won't appear despite setting it to Active later on)

--- Assets/Scripts/RoundSystem.cs
+++ Assets/Scripts/RoundSystem.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections;
 using System;
 using Unity.VisualScripting;
+using UnityEngine.TextCore.Text;
+using UnityEditor.Rendering;
 
 public class RoundSystem : MonoBehaviour
 {
@@ -10,13 +12,18 @@ public class RoundSystem : MonoBehaviour
     public float roundDuration = 60f;
 
     private int currentRound = 0;
+    private float TextOnScreen = 3f;
     public static bool End = false;
+    private GameObject Temp1;
+    private GameObject Temp2;
+    private TextMesh TextCounter;
 
     public void StartRounds()
     {
         if (currentRound <= numberOfRounds) 
         {
             currentRound++;
+            StartCoroutine(SpawnText());
             //Retour aux positions initiales
             StartCoroutine(RoundTimer());
         }
@@ -25,9 +32,23 @@ public class RoundSystem : MonoBehaviour
             //Insérer la fin du jeu
         }
     }
+
+    public void Start()
+    {
+        Temp1 = GameObject.Find("Text Round Counter");
+        Temp2 = GameObject.Find("Text Rounds");
+        Temp1.SetActive(false);
+        Temp2.SetActive(false);
+        TextCounter = GameObject.Find("Text Round Counter").GetComponent<TextMesh>();
+    }
+
     public void Update()
     {
-        if (End == true) 
+        if (Input.GetKeyDown(KeyCode.M)) 
+        {
+            StartRounds();
+        }
+        else if (End == true) 
         {
             End = false;
             StopCoroutine(RoundTimer());
@@ -35,6 +56,16 @@ public class RoundSystem : MonoBehaviour
         }
     }
     
+   private IEnumerator SpawnText()
+    {
+        TextCounter.GetComponent<TextMesh>().text = "Round " + currentRound;
+        Debug.Log(currentRound);
+        Temp1.SetActive(true);
+        Temp2.SetActive(true);
+        yield return new WaitForSeconds(TextOnScreen);
+        Temp1.SetActive(false);
+        Temp2.SetActive(false);
+    }
    private IEnumerator RoundTimer()
     {
         yield return new WaitForSeconds(roundDuration);
